bdf6721fb30805028026a5bd93cf371fb103fdcc
AnigalHub <anigal1997@gmail.com>
2022-04-12 23:06:10 +0300

    добавлен created,computed

--- src/components/account.vue
+++ src/components/account.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="pages">
-        <b-form-select class="bg-primary bg-gradient" v-model="selected" :options="options"></b-form-select>
+        <b-select class="" v-model="selected"  :options="opDates" ></b-select>
+        <div class="nameTable">Счета с остатками на дату</div>
+        <b-table :items="acctPoss" :select-mode="selectMode" selectable @row-selected="onRowSelected" :fields="fields"></b-table>
+        <div class="nameTable">Проводки</div>
+        <b-table :items="operations" :fields="fields"></b-table>
     </div>
 </template>
 
@@ -9,31 +13,47 @@
         name: "account",
         data(){
             return{
-                selected:null,
-                options:[
-                    { value: null, text: 'Please select an option' },
-                    { value: 'a', text: 'This is First option' },
-                ]
+                selected: null,
+                fields:[
+                    { key: "acctNum", label: "Номер счета" },
+                    { key: "balance", label: "Остаток" },
+                ],
+                selectMode: 'single',
             }
+        },
+        methods:{
+          onRowSelected:{
+
+          }
+        },
+        computed:{
+            opDates:function () {
+                const opDate = this.$store.getters['opDate/OpDates']
+                return opDate.map(x=>{return {text: x.OpDate, value: x.OpDate}})
+            },
+            acctPoss:function () {
+                const acctPos = this.$store.getters['acct/AcctPos']
+                let arrayCopy = []
+                for (let i in acctPos) {
+                    if (this.selected === acctPos[i].OpDate) {
+                        arrayCopy.push(acctPos[i])
+                    }
+                }
+                return arrayCopy.map(x=>{return {acctNum: x.AcctNum, balance: x.Balance}})
+            }
+        },
+        created() {
+            console.log(this.opDates)
+            this.selected = this.$store.getters['opDate/LastOpDate']
         }
     }
 </script>
 
 <style scoped lang="scss">
-    select {
-        cursor:pointer;
-        color: #fff;
-        font-size: 1.5rem;
-        padding: 1rem 1.5rem;
-        border: 0;
-        margin: 0;
-
-        text-indent: 0.25rem;
-    }
-    @media screen and (max-width: 1500px) {
-        select{
-            font-size: 1.15rem;
-        }
+    select{font-size: 1.25rem;}
+    table{font-size: 1rem;}
+    @media screen and (max-width: 1200px) {
+        select{font-size: 1rem;}
     }
 
 </style>
