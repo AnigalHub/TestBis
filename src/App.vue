778060f6b774de43dc44ef739f63d038c725fec7
AnigalHub <anigal1997@gmail.com>
2022-04-12 23:08:37 +0300

    добавлены стили

--- src/App.vue
+++ src/App.vue
@@ -28,19 +28,23 @@
     margin: 0 !important;
     padding: 0 !important;
   }
+  .nameTable{
+    padding-top: 1%;
+    font-size: 1.25rem;
+    font-weight: bold;
+    font-family: 'Lora', serif;
+  }
   .row{
     margin-right: 0 !important;
     margin-top: 0 !important;
   }
   .pages{
-    padding: 1rem 1rem;
+    padding: 1.5rem 0;
   }
   @media screen and (max-width: 992px) {
     .row{
       flex-direction: column;
-      .col-2,.col{
-        width: 100%;
-      }
+      .col-2,.col{ max-width: 100%;}
     }
   }
 </style>
