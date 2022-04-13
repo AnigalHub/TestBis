ff057ea101ef94c72fe6aff968832e8d4e7682ee
AnigalHub <anigal1997@gmail.com>
2022-04-13 20:51:59 +0300

    хранилище (state) для остатка по счеты

---
+++ src/store/acct.js
@@ -0,0 +1,89 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const state = () =>({
+AcctPos: [
+    {
+        AcctNum: "10201810000000010019",
+        Balance: 290.0,
+        OpDate: "2019-07-12"
+    },
+    {
+        AcctNum: "47441810000200000020",
+        Balance: -90.0,
+        OpDate: "2019-07-12"
+    },
+    {
+        AcctNum: "47443810000200000023",
+        Balance: -200.0,
+        OpDate: "2019-07-12"
+    },
+    {
+        AcctNum: "10201810000000010019",
+        Balance: 970.0,
+        OpDate: "2019-07-15"
+    },
+    {
+        AcctNum: "47441810000200000020",
+        Balance: -470.0,
+        OpDate: "2019-07-15"
+    },
+    {
+        AcctNum: "47443810000200000023",
+        Balance: -500.0,
+        OpDate: "2019-07-15"
+    },
+    {
+        AcctNum: "10201810000000010019",
+        Balance:1000.0,
+        OpDate: "2019-07-16"
+    },
+    {
+        AcctNum: "47441810000200000020",
+        Balance: -434.0,
+        OpDate: "2019-07-16"
+    },
+    {
+        AcctNum: "47443810000200000023",
+        Balance: -116.0,
+        OpDate: "2019-07-16"
+    }
+]})
+
+const getters = {
+    AcctPos: (state) => {
+        return state.AcctPos
+    },
+}
+const mutations ={
+
+}
+const actions = {
+    filterAcctPosByDate(context,payload){
+        let arrayCopy = []
+        for (let i in context.state.AcctPos) {
+            if (payload.opDate === context.state.AcctPos[i].OpDate) {
+                arrayCopy.push(context.state.AcctPos[i])
+            }
+        }
+        return arrayCopy.map(x=>{return {acctNum: x.AcctNum, balance: x.Balance}})
+    },
+    filterByDateAndAccount(context,payload){
+        let arrayCopy = []
+        for (let i in context.state.AcctPos){
+            if (payload.opDate === context.state.AcctPos[i].OpDate && (context.state.AcctPos[i].AcctNum === payload.acctNumCr || context.state.AcctPos[i].AcctNum === payload.acctNumDB)){
+                arrayCopy.push(context.state.AcctPos[i])
+            }
+        }
+        return arrayCopy.map(x=>{return {acctNum: x.AcctNum, balance: x.Balance}})
+    }
+}
+export default {
+    namespaced: true,
+    state,
+    actions,
+    getters,
+    mutations,
+}
