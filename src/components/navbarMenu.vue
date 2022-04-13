a0d5d7ade10e4b969cfb6279b0fe8c5eb5950baa
AnigalHub <anigal1997@gmail.com>
2022-04-13 20:50:31 +0300

    добавлены  @media

--- src/components/navbarMenu.vue
+++ src/components/navbarMenu.vue
@@ -24,30 +24,14 @@
         a {
             text-decoration: none ;
             font-family: 'Lora', serif;
-            font-size: 2rem;
-            padding: 1rem 1.5rem;
+            font-size: 1.25rem;
+            padding: .5rem .5rem;
             color: white;
             &:hover{
-                padding-left: 3.5rem;
+                padding-left: 1.5rem;
                 color: white;
                 text-decoration: underline;
             }
-        }
-    }
-    @media screen and (min-width: 1200px) and (max-width: 1500px) {
-        nav a {
-            font-size: 1.5rem;
-            padding: 1rem 1rem;
-            &:hover{
-                padding-left: 1rem
-            }
-        }
-    }
-    @media screen and (max-width: 1200px) {
-        nav a {
-            font-size: 1.3rem;
-            padding: .8rem .5rem;
-            &:hover{padding-left: .8rem}
         }
     }
     @media screen and (max-width: 992px) {
