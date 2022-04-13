fc8c7e1bfbc72ab3141fe06d0e0d9d26c9357746
AnigalHub <anigal1997@gmail.com>
2022-04-13 20:50:16 +0300

    добавлены проводки и соответствующие счета проводок

--- src/components/operations.vue
+++ src/components/operations.vue
@@ -1,12 +1,53 @@
 <template>
     <div class="pages">
-
+        <div class="nameTable">Проводки</div>
+        <b-table :items="operations" :select-mode="selectMode" selectable  @row-selected="onRowSelected" :fields="fieldsOperations"></b-table>
+        <div class="nameTable" v-if="selected" >Счета проводок</div>
+        <b-table  v-if="selected" :items="accountsOperation" :fields="fieldsAccountsOperation"></b-table>
     </div>
 </template>
 
 <script>
     export default {
-        name: "operations"
+        name: "operations",
+        data(){
+            return{
+                selected: null,
+                accountsOperation:null,
+                fieldsOperations:[
+                    { key: "opDate", label: "Дата операционного дня" },
+                    { key: "acctNumCr", label: "Счет дебета" },
+                    { key: "acctNumDB", label: "Счет кредита" },
+                    { key: "amount", label: "Сумма" },
+                ],
+                fieldsAccountsOperation:[
+                    { key: "acctNum", label: "Номер счета" },
+                    { key: "balance", label: "Остаток" },
+                ],
+                selectMode: 'single',
+            }
+        },
+        methods:{
+            onRowSelected: function(items){
+                if(items.length <= 0) {
+                    this.selected = null
+                    return
+                }
+                this.selected = items[0]
+            }
+        },
+        computed:{
+            operations:function () {
+                const opDate = this.$store.getters['operations/OpEntry']
+                return opDate.map(x=>{return {opDate: x.OpDate,acctNumCr: x.AcctNumCr,acctNumDB:x.AcctNumDB,amount:x.Amount}})
+            },
+        },
+        watch:{
+            selected: async function (val) {
+                if(val)
+                    this.accountsOperation = await this.$store.dispatch('acct/filterByDateAndAccount',val)
+            },
+        },
     }
 </script>
 
