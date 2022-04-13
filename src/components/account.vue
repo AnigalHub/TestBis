8add56124ae679fb77313c5847581f50683a14fa
AnigalHub <anigal1997@gmail.com>
2022-04-13 20:46:49 +0300

    добавлены две таблицы: счетов и соответствующие операции

--- src/components/account.vue
+++ src/components/account.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="pages">
         <b-select class="" v-model="selected"  :options="opDates" ></b-select>
+        <b-button class="mt-4" variant=" success">Создать</b-button>
         <div class="nameTable">Счета с остатками на дату</div>
-        <b-table :items="acctPoss" :select-mode="selectMode" selectable @row-selected="onRowSelected" :fields="fields"></b-table>
-        <div class="nameTable">Проводки</div>
-        <b-table :items="operations" :fields="fields"></b-table>
+        <b-table :items="acctPoss" :select-mode="selectMode" selectable @row-selected="onRowSelected" :fields="fields">
+            <template #cell(actions)="{item}">
+                <b-button class="mx-2" variant="primary">Редактировать</b-button>
+                <b-button variant="danger">Удалить</b-button>
+            </template>
+        </b-table>
+        <div class="nameTable" v-if="selectedAcc">Проводки</div>
+        <b-table v-if="selectedAcc" :items="operations" :fields="fieldsOperations"></b-table>
     </div>
 </template>
 
@@ -14,16 +20,30 @@
         data(){
             return{
                 selected: null,
+                selectedAcc: null,
+                acctPoss:null,
+                operations:null,
                 fields:[
                     { key: "acctNum", label: "Номер счета" },
                     { key: "balance", label: "Остаток" },
+                    {key:"actions", label: "Действия"}
+                ],
+                fieldsOperations:[
+                    { key: "opDate", label: "Дата операционного дня" },
+                    { key: "acctNumCr", label: "Счет дебета" },
+                    { key: "acctNumDB", label: "Счет кредита" },
+                    { key: "amount", label: "Сумма" },
                 ],
                 selectMode: 'single',
             }
         },
         methods:{
-          onRowSelected:{
-
+          onRowSelected: function(items){
+              if(items.length <= 0) {
+                  this.selectedAcc = null
+                  return
+              }
+              this.selectedAcc = items[0]
           }
         },
         computed:{
@@ -31,29 +51,23 @@
                 const opDate = this.$store.getters['opDate/OpDates']
                 return opDate.map(x=>{return {text: x.OpDate, value: x.OpDate}})
             },
-            acctPoss:function () {
-                const acctPos = this.$store.getters['acct/AcctPos']
-                let arrayCopy = []
-                for (let i in acctPos) {
-                    if (this.selected === acctPos[i].OpDate) {
-                        arrayCopy.push(acctPos[i])
-                    }
-                }
-                return arrayCopy.map(x=>{return {acctNum: x.AcctNum, balance: x.Balance}})
+        },
+        watch:{
+            selected: async function (val) {
+                this.acctPoss = await this.$store.dispatch('acct/filterAcctPosByDate', {opDate:val})
+            },
+            selectedAcc: async function (selAccount){
+                if(selAccount)
+                this.operations = await this.$store.dispatch('operations/filterByAccountAndDate',{account: selAccount.acctNum, date: this.selected})
             }
         },
         created() {
-            console.log(this.opDates)
             this.selected = this.$store.getters['opDate/LastOpDate']
         }
     }
 </script>
 
 <style scoped lang="scss">
-    select{font-size: 1.25rem;}
-    table{font-size: 1rem;}
-    @media screen and (max-width: 1200px) {
-        select{font-size: 1rem;}
-    }
+
 
 </style>
