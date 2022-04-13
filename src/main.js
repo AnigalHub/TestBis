61ced24ae458aeb3fe67703f0c9c6852789e4bb8
AnigalHub <anigal1997@gmail.com>
2022-04-13 20:51:03 +0300

    добавлены  BootstrapVue, VueRouter,vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import {BootstrapVue} from 'bootstrap-vue';
+import VueRouter from 'vue-router'
+
+
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue);
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
+import routes from './routes';
+
+import vuex from 'vuex'
+Vue.use(vuex)
+import opDate from './store/opDate'
+import acct from './store/acct'
+import operations from './store/operations'
+
+const store = new vuex.Store({
+    modules:{
+        opDate: opDate,
+        acct: acct,
+        operations:operations
+    }
+})
+
+const router = new VueRouter(
+    {
+      mode:'history',
+      routes
+    });
 
 new Vue({
   router,
