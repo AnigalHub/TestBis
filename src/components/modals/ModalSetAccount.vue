1c31b09c4d655c6fcb27345253e2b834f3e15978
AnigalHub <anigal1997@gmail.com>
2022-04-14 20:02:59 +0300

    добавлено очищение форм, isEdit - если редактирование, передаем через value объект, делаем копию или очищение

--- src/components/modals/ModalSetAccount.vue
+++ src/components/modals/ModalSetAccount.vue
@@ -1,23 +1,23 @@
 <template>
     <b-modal v-model="modalShow" hide-footer>
-        <b-form @submit.prevent="onAddAcc">
+        <b-form @submit.prevent="onSetAcc">
             <b-input-group prepend="Номер счета" class="mb-2">
-                <b-form-input v-model="newAccount.AcctNum"></b-form-input>
+                <b-form-input v-model="newAccount.AcctNum" :readonly="isEdit"></b-form-input>
             </b-input-group>
             <b-input-group prepend="Остаток" class="mb-2">
                 <b-form-input v-model="newAccount.Balance"></b-form-input>
             </b-input-group>
             <b-input-group prepend="Дата" class="mb-2">
-                <b-form-select :options="opDates" v-model="newAccount.OpDate"></b-form-select>
+                <b-form-select :options="opDates" v-model="newAccount.OpDate" :disabled="isEdit"></b-form-select>
             </b-input-group>
-            <b-button type="submit" class="mt-2" variant="success" >Добавить</b-button>
+            <b-button type="submit" class="mt-2" variant="success" >Сохранить</b-button>
         </b-form>
     </b-modal>
 </template>
 
 <script>
     export default {
-        name: "modalCreateAccount",
+        name: "modalSetAccount",
         data(){
             return{
                 modalShow:false,
@@ -29,18 +29,22 @@
             }
         },
         props:{
-          value: Boolean
+            value: Object,
+            showModal: Boolean,
+            isEdit: {
+                type: Boolean,
+                default: false,
+            },
         },
         methods:{
-            onAddAcc:async function(){
+            onSetAcc:async function(){
                 try{
                     if (this.newAccount.AcctNum !== '' && this.newAccount.Balance !== '' && this.newAccount.OpDate !== ''){
-                        await this.$store.dispatch('acct/addAccount', this.newAccount)
-                        this.modalShow = false
-                        this.$emit('addedAccount')
+                        await this.$store.dispatch('acct/setAccount', {old: this.value, new: this.newAccount})
                         this.newAccount.AcctNum = ''
                         this.newAccount.Balance = ''
                         this.newAccount.OpDate = ''
+                        this.modalShow = false
                     }
                 }
                 catch (e) {
@@ -55,8 +59,21 @@
             }
         },
         watch:{
-            value:function (val) {
+            showModal:function (val) {
                 this.modalShow = val
+            },
+            value: function(val){
+                if(val){
+                    const copy = JSON.parse(JSON.stringify(val))
+                    this.newAccount = copy
+                } else{
+                    this.newAccount = {
+                            AcctNum:'',
+                            Balance:'',
+                            OpDate:''
+                        }
+                }
+
             },
             modalShow:function (val) {
                 if(!val)
